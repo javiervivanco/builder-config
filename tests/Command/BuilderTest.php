1588832b4177b2f2816ea12b8944ee9493e0aac1
javier vivanco <javier.vivanco@teracode.com>
2015-04-30 12:18:03 -0300

    test files

--- tests/Command/BuilderTest.php
+++ tests/Command/BuilderTest.php
@@ -7,17 +7,25 @@ use JavierVivanco\BuilderConfig\Command;
 
 class BuilderTest extends  \PHPUnit_Framework_TestCase
 {
+    protected $application;
+    protected $command;
+    public function setUp(){
+        $this->application = new Application();
+        $this->application->add(new Command\BuilderCommand());
+        $this->command = $this->application->find('build');
+
+    }
+
     public function testExecute()
     {
-        $application = new Application();
-        $application->add(new Command\BuilderCommand());
-
-        $command = $application->find('build');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array('build' => $command->getName()));
-
+        $commandTester = new CommandTester($this->command);
+        array()/*array('build' => $command->getName())*/
+        $args['role'] = FILE_ROLE_TEST;
+        $args['dir']  = DIR_TEMPLATE;
+        $commandTester->execute($args);
         $this->assertRegExp('/.../', $commandTester->getDisplay());
+        $display = $commandTester->getDisplay();
+        
 
-        // ...
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
+
+define('FILE_ROLE_TEST', __DIR__.'/role-testing.yml');
+define('DIR_TEMPLATE', __DIR__.'/template');
